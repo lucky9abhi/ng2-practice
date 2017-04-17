631c46b54b4045978e1195fc0286826238746ccf
Abhi Abz <abhiui18@gmail.com>
2017-04-17 14:12:05 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Db1Component } from './dashboard/db1/db1.component';
+import { Db2Component } from './dashboard/db2/db2.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Db1Component,
+    Db2Component,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
