8b1bcdcd5fe25d04ece0d7b728f0e0b138bd07ee
Abhi Abz <abhiui18@gmail.com>
2017-04-20 16:12:56 -0500

    Commit 4/20

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
-import { Db1Component } from './dashboard/db1/db1.component';
-import { Db2Component } from './dashboard/db2/db2.component';
-import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
+
+
+const appRoutes: Routes = [
+  {path: "", component:HomeComponent},
+  {path: "login", component:LoginComponent },
+  {path: "register", component: RegisterComponent},
+  {path: "dashboard", component: DashboardComponent},
+  {path: "navbar", component: NavbarComponent},
+  {path: "profile", component: ProfileComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    Db1Component,
-    Db2Component,
+    NavbarComponent,
+    RegisterComponent,
+    LoginComponent,
+    DashboardComponent,
+    ProfileComponent,
     HomeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
